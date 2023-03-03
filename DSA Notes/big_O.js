44b2a255946dc4417649d954afb9bc235e6d1284
Yijin Ro <yri.carmine@gmail.com>
2023-03-03 15:06:47 -0500

    organized folder

---
+++ DSA Notes/big_O.js
@@ -0,0 +1,11 @@
+// notation is used to describe the time complexity and space complexity of algorithm
+
+// common complexities from the fastest to the slowest
+// 1. Constant: O(1)
+// 2. Logarithmic: O(log(n))
+// 3. Linear: O(n)
+// 4. Log-linear: O(nlog(n))
+// 5. Quadratic: O(n2)
+// 6. Cubic: O(n3)
+// 7. Exponential: O(2n)
+// 8. Factorial: O(n!)
